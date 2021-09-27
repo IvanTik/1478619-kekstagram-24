d663ddb6c822111c15cc177283173838d552bf2e
IvanTik <vania.pro2013@yandex.ru>
2021-09-27 21:22:49 +0300

    Выполнил второе задание

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-console */
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+getRandomInt(1, 10);
+
+console.log(getRandomInt(1,10));
+
+function stringLenght(string, maxlength) {
+  return (string.length > maxlength);
+}
+stringLenght('Hello');
+console.log(stringLenght('Hello'));
