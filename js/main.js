3bc3ba373303892d299e0626ef92e051f0e914df
IvanTik <vania.pro2013@yandex.ru>
2021-09-28 21:14:33 +0300

    Выполнил второе задание(1)

--- js/main.js
+++ js/main.js
@@ -3,15 +3,19 @@
 const getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
+  if (min > max) {
+    return null;
+  }
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 getRandomInt(1, 10);
 
-console.log(getRandomInt(1,10));
+console.log(getRandomInt(1, 10));
 
 function stringLenght(string, maxlength) {
-  return (string.length > maxlength);
+  return (string.length < maxlength);
 }
-stringLenght('Hello');
-console.log(stringLenght('Hello'));
+stringLenght('Hello', 7);
+
+console.log(stringLenght('Hello', 7));
